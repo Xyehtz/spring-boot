2af191227ea5cb8a47d099dd2f0519b226949781
Alejandro Garzon <alejitogarbo@gmail.com>
2024-09-26 17:57:09 -0400

    init

---
+++ src/test/java/ca/sheridancollege/garzonba/spring_learning/SpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.garzonba.spring_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
